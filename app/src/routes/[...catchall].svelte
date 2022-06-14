dcf4b6f85501a8dcb5d8cc43527ae606122fabfe
Gary Rutland <me@garyrutland.com>
2022-06-14 19:53:15 +0100

    Added base href

--- app/src/routes/[...catchall].svelte
+++ app/src/routes/[...catchall].svelte
@@ -3,14 +3,20 @@
 
     /** @type {import('./__types/[slug]').Load} */
     export async function load({params, fetch}) {
-        const url = `${!browser ? 'http://proxy' : ''}/module/${params.catchall}/`;
+        const url = `${!browser ? 'http://proxy' : ''}/module/${params.catchall}`;
         const options = {headers: {'User-Agent': 'harmony-core'}};
 
         const response = await fetch(url, options);
 
+        let dirname = params.catchall;
+        if (dirname.indexOf('.') > 0) {
+            dirname = dirname.substring(0, dirname.indexOf('/'));
+        }
+
         return {
             status: response.status,
             props: {
+                base: dirname,
                 html: await response.text(),
             }
         };
@@ -18,7 +24,11 @@
 </script>
 
 <script>
-    export let html;
+    export let base, html;
 </script>
+
+<svelte:head>
+    <base href="/{base}/"/>
+</svelte:head>
 
 {@html html}
