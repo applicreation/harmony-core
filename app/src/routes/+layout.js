e59c0f1477e2a4cc0e8147dd31a27e009aa497cc
Gary Rutland <me@garyrutland.com>
2022-08-26 21:51:06 +0100

    Fix to redirect trailing slash with relative path

--- app/src/routes/+layout.js
+++ app/src/routes/+layout.js
@@ -3,10 +3,6 @@ import {redirect} from '@sveltejs/kit'
 /** @type {import('./$types').LayoutLoad} */
 export function load({url}) {
     if (url.pathname.indexOf('.') === -1 && url.pathname.slice(-1) !== '/') {
-        const redirectUrl = new URL(url.toString())
-
-        redirectUrl.pathname += '/'
-
-        throw redirect(301, redirectUrl.toString())
+        throw redirect(301, url.pathname + '/' + url.search)
     }
 }
