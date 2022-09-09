0cff0ffcedb8fd3ff65ed299d4ae628a9ab4e4fe
Gary Rutland <me@garyrutland.com>
2022-09-09 08:06:49 +0100

    Moved module details to footer

--- app/src/routes/modules/[...catchall]/+page.svelte
+++ app/src/routes/modules/[...catchall]/+page.svelte
@@ -4,28 +4,40 @@
 </script>
 
 <div class="h-100 d-flex flex-column">
-    <div class="p-3 d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center border-bottom">
+    <div class="p-3 border-bottom">
         <h1 class="h2 m-0">{data.module.name}</h1>
-        <div>
-            <a href="https://github.com/{data.module.github.owner}/{data.module.github.name}/releases"
-               class="text-decoration-none" target="_blank">
-                <span class="badge bg-secondary me-1">{data.module.version}</span>
-                {#if data.module._computed.version.upgrade === 'misconfigured' }
-                    <span class="badge text-bg-danger">Misconfigured version</span>
-                {:else if data.module._computed.version.upgrade === 'patch' }
-                    <span class="badge text-bg-success">Patch update available</span>
-                {:else if data.module._computed.version.upgrade === 'minor'}
-                    <span class="badge text-bg-warning">Minor update available</span>
-                {:else if data.module._computed.version.upgrade === 'major'}
-                    <span class="badge text-bg-danger">Major update available</span>
-                {:else if data.module._computed.version.upgrade === 'unknown'}
-                    <span class="badge text-bg-secondary">Unknown version</span>
-                {/if}
-            </a>
-        </div>
     </div>
 
     <div class="flex-grow-1">
         {@html data.html}
+    </div>
+
+    <div class="p-3 border-top">
+        <div>
+            <a href="https://github.com/{data.module.github.owner}/{data.module.github.name}/tree/{data.module.version}"
+               class="btn btn-secondary btn-sm" target="_blank">
+                {data.module.version}
+            </a>
+            <a href="https://github.com/{data.module.github.owner}/{data.module.github.name}/releases" target="_blank"
+               class="btn btn-secondary btn-sm">
+                Releases
+            </a>
+            <a href="https://github.com/{data.module.github.owner}/{data.module.github.name}/issues" target="_blank"
+               class="btn btn-secondary btn-sm">
+                Issues
+            </a>
+        </div>
+
+        {#if data.module._computed.version.upgrade === 'misconfigured' }
+            <span class="badge text-bg-danger">Misconfigured version</span>
+        {:else if data.module._computed.version.upgrade === 'patch' }
+            <span class="badge text-bg-success">Patch update available</span>
+        {:else if data.module._computed.version.upgrade === 'minor'}
+            <span class="badge text-bg-warning">Minor update available</span>
+        {:else if data.module._computed.version.upgrade === 'major'}
+            <span class="badge text-bg-danger">Major update available</span>
+        {:else if data.module._computed.version.upgrade === 'unknown'}
+            <span class="badge text-bg-secondary">Unknown version</span>
+        {/if}
     </div>
 </div>
